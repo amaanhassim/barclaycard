3e416a01047f8e4668b15433fe694dc3b86056ee
LloydCrawford <lloydcrawford@virginmedia.com>
2021-02-08 13:42:13 +0000

    appointments

---
+++ public/userAppointment.php
@@ -0,0 +1,46 @@
+<?php
+session_start()
+require '..functions/db.php';
+
+if (isset($_SESSION['0'])){
+
+    $query = $pdo->prepare('SELECT location FROM locations');
+    $query->execute();
+
+    foreach ($query as $data) {
+        $location = $location . '<option> <a href="' . $data['name'] . '"> '. $data['name'] .'</a></option>';
+    } //print each value from the table in the desired layout
+ 
+
+    $query = $pdo->prepare('SELECT time FROM timeSlots WHERE avalible == 0 ');
+    $query->execute();
+
+    foreach ($query as $data) {
+        $time = $time . '<option> <a href="' . $data['name'] . '"> '. $data['name'] .'</a></option>';
+    } //print each value from the table in the desired layout
+
+
+    if(isset($_POST['submit'])){
+        $appointment = $pdo->prepare('UPDATE appointmenst SET name = :update1 WHERE name = :category2 ');
+        $catNName = [
+            'name' => $_POST['name'],
+            'time' => $_POST['timeSlot']
+        ];
+        $upCat->execute($catNName);
+
+
+
+$content = '
+    <form action="article.php" method="post">
+                    <label> location </labe> <select name = "location">
+                    '. $location .'
+                    </select>
+                    <label> Time Slot </labe> <input type="text" name = "timeSlot"/>
+                    <input type="submit" name="submit3" value="Submit" style="margin-left: 0px"/>
+                </form>
+
+';
+
+
+}
+?>
