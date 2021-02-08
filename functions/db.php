be730039d8e0a31893219fd10265130a5c0b9781
jamie-code <56395835+jamie-code@users.noreply.github.com>
2021-02-08 13:21:34 +0000

    new db info

--- functions/db.php
+++ functions/db.php
@@ -1,8 +1,8 @@
 <?php
-$server= 'v.je';
-$username = 'student';
-$password = 'student';
-$schema = 'csy2028';
+$server= 'jamiez.co.uk';
+$username = 'barclaycard';
+$password = 'btVIIt4xJ6zJ0J4I';
+$schema = 'barclaycard';
 
 $pdo = new PDO('mysql:dbname='.$schema.';host='.$server,$username,$password,
             [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
