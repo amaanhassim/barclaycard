ccd4960c8c11f5984970d818c0b288d49edb5dbb
Alex <alexardeleanu2001@gmail.com>
2021-02-08 13:20:12 +0000

    commit add user changes

--- public/addUser.php
+++ public/addUser.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 session_start(); 
-
+require '..functions/'
+require '../functions/loadtemplate.php';
 
 
 
@@ -15,17 +16,11 @@ $values = [
  'access_level' => '0',
  ];
 $stmt->execute($values);
-echo '<h2>Thank you for registering on our site!<h2>',
-'<h3>We will post news and daily articles on our town<h3>',
-'<h3>for you to enjoy reading and stay up to date!<h3>',
-'<h3>You can do so by logging in to your account<h3>',
-'<h3>right here! <a href="/login.php"> Log in </a></h3>';
-// when signing up the user will endter theyr details as listed and the access_level will
-// be automaticaly set to 0 - normal user which admins can change in the edit page
+
 }
 else {
+    $content = loadtemplate('../templates/sampleform.html.php', []);
+
+    require '../templates/layout.html.php';
+}
 ?>
-
-
-
-<?php }
