b067a47c11c9f91444dcf50277109e948ba1181f
amaanhassim <45460942+amaanhassim@users.noreply.github.com>
2021-02-08 14:29:06 +0000

    commit

--- public/admin/addproducts.php
+++ public/admin/addproducts.php
@@ -1,11 +1,20 @@
 <?php
 
 require '../../functions/loadtemplate.php';
+require '../../functions/amdbconnection.php';
 session_start();
 
 if(isset($_POST['submit']))
 {
+    $stmt = $pdo2->prepare('INSERT INTO products(productname,productprice,description)
+                            VALUES(:productname, productprice, description)');
     
+    $values = [
+        'productname' => $_POST['productname'],
+        'productprice' => $_POST['productprice'],
+        'description' => $_POST['description']
+    ];
+    $stmt->execute($values);
 }
 
 $content = loadtemplate('../../templates/addproducts.html.php', []);
