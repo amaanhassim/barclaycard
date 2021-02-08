b6265d5470548ae3eed59d00f412d61b033d6ba1
Alex <alexardeleanu2001@gmail.com>
2021-02-08 14:19:51 +0000

    addUser, login commit

--- public/addUser.php
+++ public/addUser.php
@@ -4,8 +4,8 @@ require '../functions/db.php';
 require '../functions/loadtemplate.php';
 
 
-if (isset($_POST['signup'])) {
-$stmt = $pdo->prepare('INSERT INTO Users (firstname, surname, email, pass, access_level)
+if (isset($_POST['submit'])) {
+$stmt = $pdo->prepare('INSERT INTO users (firstname, surname, email, pass, access_level)
  VALUES (:firstname, :surname, :email, :pass, :access_level)
 ');
 $values = [
@@ -23,14 +23,21 @@ else {
     $content = ('<hr />
     <h1>Register</h1>
     
-    <div class="signup">
-    <form action="#">
-        <label>Firstname</label> <input type="text" />
-        <label>Surname</label> <input type="text" />
-        <label>Email</label> <input type="text" />
-        <label>Password</label> <input type="password" />
-        <input type="checkbox"> Recive emails with new products </input> 
-        <input type="submit" value="Submit" />
+    <div class="addUser">
+    <form action="addUser.php" method="POST">
+        <label>Firstname</label>
+         <input name="firstname" type="text" />
+
+        <label>Surname</label> 
+        <input name="surname" type="text" />
+
+        <label>Email</label> 
+        <input name="email" type="text" />
+
+        <label>Password</label> 
+        <input name="pass" type="password" />
+
+        <input name="submit" type="submit" value="Submit" />
 
     </form>
     </div>');
