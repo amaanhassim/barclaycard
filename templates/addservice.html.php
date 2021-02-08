82ca2a68faba09c94055a5540c250c26558c6c70
jamie-code <56395835+jamie-code@users.noreply.github.com>
2021-02-08 16:40:31 +0000

    removed images

--- templates/addservice.html.php
+++ templates/addservice.html.php
@@ -5,7 +5,6 @@
 						<label>Service Name</label> <input type="text" name="service_name"/>
 						<label>Description</label> <input type="text" name="service_desc" />
 						<label>Price (£)</label> <input type="text" name="service_price" />
-						<label>Image</label> <input type="submit" name="service_image" />
 						<input type="submit" value="Submit" />
 
 					</form>
