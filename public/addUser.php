dc1cb7c406f89b5c65c542f6f0c15a9363eb8576
Alex <alexardeleanu2001@gmail.com>
2021-02-08 13:47:54 +0000

    add user

--- public/addUser.php
+++ public/addUser.php
@@ -1,16 +1,16 @@
 <?php
 session_start(); 
-require '..functions/db.php';
+require '../functions/db.php';
 require '../functions/loadtemplate.php';
 
 
-
 if (isset($_POST['signup'])) {
-$stmt = $pdo->prepare('INSERT INTO Users (username, email, pass, access_level)
- VALUES (:username, :email, :pass, :access_level)
+$stmt = $pdo->prepare('INSERT INTO Users (firstname, surname, email, pass, access_level)
+ VALUES (:firstname, :surname, :email, :pass, :access_level)
 ');
 $values = [
- 'username' => $_POST['username'],
+ 'firstname' => $_POST['firstname'],
+ 'surname' => $_POST['surname'],
  'email' => $_POST['email'],
  'pass' => $_POST['pass'],
  'access_level' => '0',
@@ -19,7 +19,21 @@ $stmt->execute($values);
 
 }
 else {
-    $content = loadtemplate('../templates/sampleform.html.php', []);
+
+    $content = ('<hr />
+    <h1>Register</h1>
+    
+    <div class="signup">
+    <form action="#">
+        <label>Firstname</label> <input type="text" />
+        <label>Surname</label> <input type="text" />
+        <label>Email</label> <input type="text" />
+        <label>Password</label> <input type="password" />
+        <input type="checkbox"> Recive emails with new products </input> 
+        <input type="submit" value="Submit" />
+
+    </form>
+    </div>');
 
     require '../templates/layout.html.php';
 }
